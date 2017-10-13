76468a5b8d1fab2cc2d1900ee8c47537e4b2c985
Ashlee Crusco <ashleecrusco@gmail.com>
2017-10-13 17:50:04 -0400

    added tag show

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,10 @@ class TagsController < ApplicationController
     @tags = Tag.all.sort_by {|tag| tag.name}
     erb :'tags/index'
   end
+
+  get '/tags/:id' do
+    @tag = Tag.find(params[:id])
+    @pictures = @tag.pictures
+    erb :'tags/show'
+  end
 end
