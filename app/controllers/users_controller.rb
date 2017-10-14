28d2a2e797a5167c129c7be7f7b85ab332b23b9d
Ashlee Crusco <ashleecrusco@gmail.com>
2017-10-14 10:14:02 -0400

    added user edit/delete functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,40 @@ class UsersController < ApplicationController
     erb :'users/index'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    new_user = User.create(params)
+    new_user.save
+    redirect "/users/#{new_user.id}"
+  end
+
   get '/users/:id' do
     @user = User.find(params[:id])
     @pictures = @user.pictures
     erb :'users/show'
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    erb :'users/edit'
+  end
+
+  patch '/users/:id' do
+    @user = User.find(params[:id])
+    @user.update_attributes(params["user"])
+    redirect "/users/#{@user.id}"
+  end
+
+  get '/users/:id/delete' do
+    @user = User.find(params[:id])
+  end
+
+  delete '/users/:id/delete' do
+    @user = User.find(params[:id])
+    @user.delete
+    redirect "/users"
   end
 end
